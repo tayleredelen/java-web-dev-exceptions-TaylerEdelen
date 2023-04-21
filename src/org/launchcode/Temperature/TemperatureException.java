cd702a01b1f39ed8cc4b25cb99562630fec6c3d8
tayleredelen <taylerledelen@gmail.com>
2023-04-21 13:16:56 -0700

    Finished exercise

--- src/org/launchcode/Temperature/TemperatureException.java
+++ src/org/launchcode/Temperature/TemperatureException.java
@@ -1,5 +1,16 @@
 package org.launchcode.Temperature;
 
-public class TemperatureException {
-    // Write code here!
+public class TemperatureException extends Exception {
+    //all custom exceptions must inherit from the Exception class.
+
+    public TemperatureException(String message){
+        super(message);
+    }
+    //With the relationship between the two classes set up, we now need to add a constructor.
+    // In this case, that is all we need to add. We do not want to call the Exception class directly,
+    // so we will use TemperatureException to access its properties.
+    // The constructor will only have to deal with a message parameter.
+
+
+
 }
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,7 +1,6 @@
 package org.launchcode.exercises;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
@@ -9,23 +8,56 @@ public class Main {
 
         // Test out your Divide() function!
 
+//        System.out.println(Divide(5,0));
+
         HashMap<String, String> studentFiles = new HashMap<>();
-        studentFiles.put("Carl", "Program.java");
-        studentFiles.put("Brad", "");
-        studentFiles.put("Elizabeth", "MyCode.java");
-        studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Carl", "Program.ts"); //changed to ts to test 0 point return
+        studentFiles.put("Brad", ""); //throws error "You must submit a file"
+        studentFiles.put("Elizabeth", "MyCode.java"); //1 point return
+        studentFiles.put("Stefanie", "CoolProgram.java"); //1 point return
 
         // Test out your CheckFileExtension() function!
+
+        for (String student : studentFiles.keySet()) {
+            int points = CheckFileExtension(studentFiles.get(student));
+            System.out.println(student + ": " + points + " points");
+        }
+
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    //If y is zero, you should throw an exception.
+    public static int Divide(int x, int y) {
+        if (y == 0) {
+            try {
+                throw new ArithmeticException("You cannot divide by 0!");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        return (x / y);
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+//    If a student’s submitted file ends in .java, they get 1 point.
+//    If a student’s submitted file doesn’t end in .java, they get 0 points.
+//    If the file submitted is null or an empty string,
+//    an exception should be thrown and you should give the student -1 points.
+    public static int CheckFileExtension(String fileName) {
+        int numPoints = 0;
+        if (fileName.contains(".java")) {
+            numPoints += 1;
+        }
+                if (fileName == null || fileName == ""){
+                    try {
+                        throw new Exception("You must submit a file.");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    numPoints -= 1;
+            }
+        return numPoints;
     }
+
+
 
 }
+
